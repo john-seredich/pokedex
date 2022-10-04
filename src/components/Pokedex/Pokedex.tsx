fc2f5a2dc9c1b8007aa45440b26f7844db22a70e
john-seredich <seredichjohnalt@gmail.com>
2022-10-04 13:13:34 -0400

    Feat: Basic query function v.2

--- src/components/Pokedex/Pokedex.tsx
+++ src/components/Pokedex/Pokedex.tsx
@@ -1,4 +1,14 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const fetchPokemonList = () => {
+  const request = axios
+    .get("https://pokeapi.co/api/v2/pokemon?offset=0&limit=20")
+    .then((response) => response.data);
+};
+
 function Pokedex() {
+  fetchPokemonList();
   return <div>Pokedex</div>;
 }
 
