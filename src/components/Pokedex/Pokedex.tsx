9f71c21286656d4dd0c4ba5082380b9b1ee54d94
john-seredich <seredichjohnalt@gmail.com>
2022-10-04 13:39:46 -0400

    feat: fetch pokemon with load more button

--- src/components/Pokedex/Pokedex.tsx
+++ src/components/Pokedex/Pokedex.tsx
@@ -1,17 +1,51 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-const fetchPokemonList = async () => {
-  const request = await axios.get(
-    "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20"
-  );
+interface pokemonListTypes {
+  name: string;
+  url: string;
+}
+
+const fetchPokemonList = async ({
+  pageParam = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20",
+}) => {
+  const request = await axios.get(pageParam);
   const { results, next } = await request.data;
   return { response: results, nextPage: next };
 };
 
 function Pokedex() {
-  fetchPokemonList();
-  return <div>Pokedex</div>;
+  const {
+    isLoading,
+    isError,
+    error,
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery(["pokemon"], fetchPokemonList, {
+    getNextPageParam: (lastPage) => lastPage.nextPage,
+  });
+  return (
+    <div>
+      <h1>Pokedex</h1>
+      <div>
+        {data?.pages.map((group, i) =>
+          group.response.map((pokemon: pokemonListTypes) => (
+            <li key={pokemon.name}>{pokemon.name}</li>
+          ))
+        )}
+      </div>
+      <div>
+        <button
+          disabled={!hasNextPage || isFetchingNextPage}
+          onClick={() => fetchNextPage()}
+        >
+          Load More
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Pokedex;
