0fef56d5d0608da94c515bcf3ce339cb2f27bd13
john-seredich <seredichjohnalt@gmail.com>
2022-10-04 13:20:42 -0400

    refactor pokemon list fetch function with async await

--- src/components/Pokedex/Pokedex.tsx
+++ src/components/Pokedex/Pokedex.tsx
@@ -1,10 +1,12 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-const fetchPokemonList = () => {
-  const request = axios
-    .get("https://pokeapi.co/api/v2/pokemon?offset=0&limit=20")
-    .then((response) => response.data);
+const fetchPokemonList = async () => {
+  const request = await axios.get(
+    "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20"
+  );
+  const { results, next } = await request.data;
+  return { response: results, nextPage: next };
 };
 
 function Pokedex() {
