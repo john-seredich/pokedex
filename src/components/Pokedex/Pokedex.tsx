3fa9d06e584b8c04d9c1fb2a328bc67965c8751d
john-seredich <seredichjohnalt@gmail.com>
2022-10-17 15:30:46 -0400

    feat: fetch data for first 20 pokemon

--- src/components/Pokedex/Pokedex.tsx
+++ src/components/Pokedex/Pokedex.tsx
@@ -1,11 +1,17 @@
 import { useInView } from "react-intersection-observer";
 import { usePokemonsList } from "../../hooks/usePokemonList";
+import axios from "axios";
 import styles from "./Pokedex.module.scss";
+import { useQueries } from "@tanstack/react-query";
 
 interface pokemonListTypes {
   name: string;
   url: string;
 }
+
+const fetchPokemonData = (url: any) => {
+  return axios.get(url);
+};
 
 function Pokedex() {
   const {
@@ -25,19 +31,63 @@ function Pokedex() {
     fetchNextPage();
   }
 
+  // const pokemonUrl1 = data?.pages.map((group) => {
+  //   return group.response.map((pokemon: pokemonListTypes, i: number) => {
+  //     return pokemon.url;
+  //   });
+  // });
+
+  const pokemonUrl = [
+    [
+      "https://pokeapi.co/api/v2/pokemon/1/",
+      "https://pokeapi.co/api/v2/pokemon/2/",
+      "https://pokeapi.co/api/v2/pokemon/3/",
+      "https://pokeapi.co/api/v2/pokemon/4/",
+      "https://pokeapi.co/api/v2/pokemon/5/",
+      "https://pokeapi.co/api/v2/pokemon/6/",
+      "https://pokeapi.co/api/v2/pokemon/7/",
+      "https://pokeapi.co/api/v2/pokemon/8/",
+      "https://pokeapi.co/api/v2/pokemon/9/",
+      "https://pokeapi.co/api/v2/pokemon/10/",
+      "https://pokeapi.co/api/v2/pokemon/11/",
+      "https://pokeapi.co/api/v2/pokemon/12/",
+      "https://pokeapi.co/api/v2/pokemon/13/",
+      "https://pokeapi.co/api/v2/pokemon/14/",
+      "https://pokeapi.co/api/v2/pokemon/15/",
+      "https://pokeapi.co/api/v2/pokemon/16/",
+      "https://pokeapi.co/api/v2/pokemon/17/",
+      "https://pokeapi.co/api/v2/pokemon/18/",
+      "https://pokeapi.co/api/v2/pokemon/19/",
+      "https://pokeapi.co/api/v2/pokemon/20/",
+    ],
+  ];
+
+  const queryResults = useQueries({
+    queries: pokemonUrl[0].map((url, i) => {
+      return {
+        queryKey: ["pokemonData", i],
+        queryFn: () => fetchPokemonData(url),
+      };
+    }),
+  });
+
   const pokemonList = data?.pages.map((group) => {
     return group.response.map((pokemon: pokemonListTypes, i: number) => {
-      // Last element in each API call
       if (group.response.length - 1 === i) {
         return (
           <div ref={ref} key={pokemon.name} className={styles.pokedex__card}>
             <h3>{pokemon.name}</h3>
+            <img
+              src={queryResults[i].data?.data.sprites.front_default}
+              alt=""
+            />
           </div>
         );
       }
       return (
         <div key={pokemon.name} className={styles.pokedex__card}>
           <h3>{pokemon.name}</h3>
+          <img src={queryResults[i].data?.data.sprites.front_default} alt="" />
         </div>
       );
     });
