4d10eb2175b8d7a2429cd598ec112be3f04b9c9c
john-seredich <seredichjohnalt@gmail.com>
2022-10-19 14:27:56 -0400

    Feat: move pokemon info into seperate component

--- src/components/Pokedex/Pokedex.tsx
+++ src/components/Pokedex/Pokedex.tsx
@@ -2,7 +2,8 @@ import { useInView } from "react-intersection-observer";
 import { usePokemonsList } from "../../hooks/usePokemonList";
 import axios from "axios";
 import styles from "./Pokedex.module.scss";
-import { useQueries } from "@tanstack/react-query";
+import { useState } from "react";
+import PokemonCard from "../PokemonCard/PokemonCard";
 
 interface pokemonListTypes {
   name: string;
@@ -31,73 +32,28 @@ function Pokedex() {
     fetchNextPage();
   }
 
-  // const pokemonUrl1 = data?.pages.map((group) => {
-  //   return group.response.map((pokemon: pokemonListTypes, i: number) => {
-  //     return pokemon.url;
-  //   });
-  // });
-
-  const pokemonUrl = [
-    [
-      "https://pokeapi.co/api/v2/pokemon/1/",
-      "https://pokeapi.co/api/v2/pokemon/2/",
-      "https://pokeapi.co/api/v2/pokemon/3/",
-      "https://pokeapi.co/api/v2/pokemon/4/",
-      "https://pokeapi.co/api/v2/pokemon/5/",
-      "https://pokeapi.co/api/v2/pokemon/6/",
-      "https://pokeapi.co/api/v2/pokemon/7/",
-      "https://pokeapi.co/api/v2/pokemon/8/",
-      "https://pokeapi.co/api/v2/pokemon/9/",
-      "https://pokeapi.co/api/v2/pokemon/10/",
-      "https://pokeapi.co/api/v2/pokemon/11/",
-      "https://pokeapi.co/api/v2/pokemon/12/",
-      "https://pokeapi.co/api/v2/pokemon/13/",
-      "https://pokeapi.co/api/v2/pokemon/14/",
-      "https://pokeapi.co/api/v2/pokemon/15/",
-      "https://pokeapi.co/api/v2/pokemon/16/",
-      "https://pokeapi.co/api/v2/pokemon/17/",
-      "https://pokeapi.co/api/v2/pokemon/18/",
-      "https://pokeapi.co/api/v2/pokemon/19/",
-      "https://pokeapi.co/api/v2/pokemon/20/",
-    ],
-  ];
-
-  const queryResults = useQueries({
-    queries: pokemonUrl[0].map((url, i) => {
-      return {
-        queryKey: ["pokemonData", i],
-        queryFn: () => fetchPokemonData(url),
-      };
-    }),
-  });
-
   const pokemonList = data?.pages.map((group) => {
     return group.response.map((pokemon: pokemonListTypes, i: number) => {
-      if (group.response.length - 1 === i) {
-        return (
-          <div ref={ref} key={pokemon.name} className={styles.pokedex__card}>
-            <h3>{pokemon.name}</h3>
-            <img
-              src={queryResults[i].data?.data.sprites.front_default}
-              alt=""
-            />
-          </div>
-        );
+      if (group.response) {
+        const info = { name: pokemon.name, url: pokemon.url };
+        if (group.response.length - 1 === i) {
+          // Return Pokemon To observer for infinite scroll
+          return (
+            <div ref={ref} key={pokemon.name} className={styles.pokedex__card}>
+              <PokemonCard {...info} />
+            </div>
+          );
+        } else {
+          // Return Non Observer Pokemon
+          return (
+            <div key={pokemon.name} className={styles.pokedex__card}>
+              <PokemonCard {...info} />
+            </div>
+          );
+        }
       }
-      return (
-        <div key={pokemon.name} className={styles.pokedex__card}>
-          <h3>{pokemon.name}</h3>
-          <img src={queryResults[i].data?.data.sprites.front_default} alt="" />
-        </div>
-      );
     });
   });
-
-  if (isLoading) return <h2>Loading...</h2>;
-  if (error instanceof Error)
-    return <h2>Something went wrong. {error.message}</h2>;
-
-  // TODO: Add a check if reaches the last page
 
   return (
     <div className={styles.pokedex}>
@@ -108,3 +64,97 @@ function Pokedex() {
 }
 
 export default Pokedex;
+
+// import { useInView } from "react-intersection-observer";
+// import { usePokemonsList } from "../../hooks/usePokemonList";
+// import axios from "axios";
+// import styles from "./Pokedex.module.scss";
+// import { useQueries } from "@tanstack/react-query";
+// import { pokemonUrlList } from "../../pokemonUrlList";
+// import React, { useState, useEffect } from "react";
+
+// interface pokemonListTypes {
+//   name: string;
+//   url: string;
+// }
+
+// const fetchPokemonData = (url: any) => {
+//   return axios.get(url);
+// };
+
+// function Pokedex() {
+//   const [pokemonUrls, setPokemonUrls] = useState(pokemonUrlList);
+//   const {
+//     isLoading,
+//     error,
+//     data,
+//     fetchNextPage,
+//     hasNextPage,
+//     isFetchingNextPage,
+//   } = usePokemonsList();
+
+//   const { ref, inView } = useInView({
+//     rootMargin: "200px",
+//   });
+
+//   if (inView) {
+//     fetchNextPage();
+//   }
+
+//   const pokemonUrl = data?.pages.map((group) => {
+//     return group.response.map((pokemon: pokemonListTypes, i: number) => {
+//       return pokemon.url;
+//     });
+//   });
+
+//   console.log(pokemonUrl);
+
+//   // const queryResults = useQueries({
+//   //   queries: pokemonUrl[0].map((url: any, i: any) => {
+//   //     return {
+//   //       queryKey: ["pokemonData", i],
+//   //       queryFn: () => fetchPokemonData(url),
+//   //       // initialData: pokemonUrl1,
+//   //       // enabled: !!pokemonUrl,
+//   //     };
+//   //   }),
+//   // });
+
+//   const pokemonList = data?.pages.map((group) => {
+//     return group.response.map((pokemon: pokemonListTypes, i: number) => {
+//       if (group.response.length - 1 === i) {
+//         // Set State, url
+//         return (
+//           <div ref={ref} key={pokemon.name} className={styles.pokedex__card}>
+//             <h3>{pokemon.name}</h3>
+//             {/* <img
+//               src={queryResults[i].data?.data.sprites.front_default}
+//               alt=""
+//             /> */}
+//           </div>
+//         );
+//       }
+//       return (
+//         <div key={pokemon.name} className={styles.pokedex__card}>
+//           <h3>{pokemon.name}</h3>
+//           {/* <img src={queryResults[i].data?.data.sprites.front_default} alt="" /> */}
+//         </div>
+//       );
+//     });
+//   });
+
+//   if (isLoading) return <h2>Loading...</h2>;
+//   if (error instanceof Error)
+//     return <h2>Something went wrong. {error.message}</h2>;
+
+//   // TODO: Add a check if reaches the last page
+
+//   return (
+//     <div className={styles.pokedex}>
+//       <div className={styles.pokedex__container}>{pokemonList}</div>
+//       {isFetchingNextPage && <h2>Loading...</h2>}
+//     </div>
+//   );
+// }
+
+// export default Pokedex;
---
+++ src/components/PokemonCard/PokemonCard.tsx
@@ -0,0 +1,10 @@
+function PokemonCard(props: any) {
+  console.log(props);
+  return (
+    <>
+      <h3>{props.name}</h3>
+    </>
+  );
+}
+
+export default PokemonCard;
