84306a0ddbc45f3f555830994089176ec4a92265
john-seredich <seredichjohnalt@gmail.com>
2022-10-19 16:22:55 -0400

    feat: new fetch function for pokemonData

---
+++ src/hooks/usePokemonData.tsx
@@ -0,0 +1,10 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const fetchPokemonData = (url: string) => {
+  return axios.get(url);
+};
+
+export function usePokemonData(url: string) {
+  return useQuery(["pokemonData", url], () => fetchPokemonData(url));
+}
