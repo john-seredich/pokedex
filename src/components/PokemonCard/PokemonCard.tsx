2782f2c9a6b2e3467985700150140ddd0a5c2867
john-seredich <seredichjohnalt@gmail.com>
2022-10-20 14:11:40 -0400

    Feat: uppercase type names, card style, refactor uppercase function

--- src/components/PokemonCard/PokemonCard.tsx
+++ src/components/PokemonCard/PokemonCard.tsx
@@ -1,27 +1,28 @@
 import { usePokemonData } from "../../hooks/usePokemonData";
 import styles from "./PokemonCard.module.scss";
 
-function useUpperCase(string: string) {
+function setUpperCase(string: string) {
   return string[0].toUpperCase() + string.slice(1);
 }
 
 function PokemonCard(props: any) {
   const { data, isLoading } = usePokemonData(props.url);
-  const img = data?.data.sprites.front_default;
-  const types = data?.data.types.map((type: any, i: number) => {
-    return <p key={i}>{type.type.name}</p>;
-  });
+  const pokemonImg = data?.data.sprites.front_default;
   const pokemonNumber = data?.data.id;
-  const pokemonName = useUpperCase(props.name);
-  console.log(data?.data);
+  const pokemonName = setUpperCase(props.name);
+  const pokemonColor = data?.data.types[0].type.name;
+  const pokemonTypes = data?.data.types.map((type: any, i: number) => {
+    const upperCaseType = setUpperCase(type.type.name);
+    return <p key={i}>{upperCaseType}</p>;
+  });
 
   return (
-    <div className={styles.pokemon_card}>
+    <div className={`${styles.pokemon_card} ${styles[pokemonColor]}`}>
       <h4>#{pokemonNumber}</h4>
       <h3>{pokemonName}</h3>
       {isLoading && <p>Loading...</p>}
-      <img src={img} alt={props.name} />
-      {types}
+      <img src={pokemonImg} alt={props.name} />
+      {pokemonTypes}
     </div>
   );
 }
