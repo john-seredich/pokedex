bb39ebcb90ae17d49e9addd98b1a0971b8bd4772
john-seredich <seredichjohnalt@gmail.com>
2022-10-20 15:10:59 -0400

    removed any from any type definitions and added proper type checks

--- src/components/PokemonCard/PokemonCard.tsx
+++ src/components/PokemonCard/PokemonCard.tsx
@@ -1,20 +1,35 @@
 import { usePokemonData } from "../../hooks/usePokemonData";
 import styles from "./PokemonCard.module.scss";
 
+interface Props {
+  name: string;
+  url: string;
+}
+
+interface pokemonType {
+  slot: string;
+  type: {
+    name: string;
+    url: string;
+  };
+}
+
 function setUpperCase(string: string) {
   return string[0].toUpperCase() + string.slice(1);
 }
 
-function PokemonCard(props: any) {
+function PokemonCard(props: Props) {
   const { data, isLoading } = usePokemonData(props.url);
   const pokemonImg = data?.data.sprites.front_default;
   const pokemonNumber = data?.data.id;
   const pokemonName = setUpperCase(props.name);
   const pokemonColor = data?.data.types[0].type.name;
-  const pokemonTypes = data?.data.types.map((type: any, i: number) => {
+  const pokemonTypes = data?.data.types.map((type: pokemonType, i: number) => {
     const upperCaseType = setUpperCase(type.type.name);
     return <p key={i}>{upperCaseType}</p>;
   });
+
+  console.log(data?.data.types);
 
   return (
     <div className={`${styles.pokemon_card} ${styles[pokemonColor]}`}>
