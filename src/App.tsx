2fb203a169f104140191a254b32f7aad99a822e1
john-seredich <seredichjohnalt@gmail.com>
2022-10-24 13:40:30 -0400

    Feat: Header component and style change

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Pokedex from "./components/Pokedex/Pokedex";
+import Header from "./layout/Header/Header";
 
 function App() {
   const queryClient = new QueryClient();
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <QueryClientProvider client={queryClient}>
+        <Header />
         <Pokedex />
         <ReactQueryDevtools initialIsOpen={true} />
       </QueryClientProvider>
---
+++ src/layout/Header/Header.tsx
@@ -0,0 +1,11 @@
+import styles from "./Header.module.scss";
+
+function Header() {
+  return (
+    <>
+      <h1 className={styles.header__title}>Pokédex</h1>
+    </>
+  );
+}
+
+export default Header;
