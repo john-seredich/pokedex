eb1a5acf909204eac7468fb6201b62b5cdf8168b
john-seredich <seredichjohnalt@gmail.com>
2022-10-25 14:17:26 -0400

    feat: search bar style and general style

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,9 +1,11 @@
+import Search from "../../components/Search/Search";
 import styles from "./Header.module.scss";
 
 function Header() {
   return (
     <>
       <h1 className={styles.header__title}>Pokédex</h1>
+      <Search />
     </>
   );
 }
