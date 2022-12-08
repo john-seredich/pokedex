c9cabf9792561bef94a234d8064fb874c378ebca
john-seredich <seredichjohnalt@gmail.com>
2022-12-08 15:48:12 -0500

    feat: close modal with click on backdrop

--- src/components/Modal/PokemonModalBody/PokemonModalBody.tsx
+++ src/components/Modal/PokemonModalBody/PokemonModalBody.tsx
@@ -6,7 +6,10 @@ import styles from "./PokemonModalBody.module.scss";
 function PokemonModalBody(props: pokemonDataProps) {
   return (
     <>
-      <div className={styles.backdrop}></div>
+      <div
+        className={styles.backdrop}
+        onClick={() => props.setIsOpen(false)}
+      ></div>
       <div className={`${styles.modal}`} onClick={() => props.setIsOpen(false)}>
         <PokemonModalHeader {...props} />
         <PokemonModalData {...props} />
